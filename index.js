7753a6184285376de3d05d480450398722b7fc77
James V <james.vanhoke@yahoo.com>
2021-03-19 19:45:58 -0700

    added required NPM and Inquirer questions

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
+//NPM That allows us to ask questions via terminal and return the user inputs
+const inquirer = require("inquirer");
+//Utility JS, uses answers to generate our readme content
+const generateMarkdown = require("./utils/generateMarkdown.js")
+//NPM that allows us to save files
+const fs = require("fs");
 
-// prompt for information about application
 
-// write user responses to template literals sample readme.MD
-//     Description, 
-//     Table of Contents, 
-//     Installation
-//     Usage
-//     License
-//     Contributing,
-//     Tests
-//     Questions
-
-// project title is displayed as the title of the README
-
-// choose a license from a list of options, print badge for that license to top of readme and add it to the license section
-
-//enter GitHub username add to the section of the README entitled Questions, with a link to the GitHub profile
-
-//enter email address add to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-//click on the links in the Table of Contents have it link to corresponding sections
+//Variable containing all the user prompts to build our readme.
+const userQuestions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "projDesc",
+        message: "Write a description for your project."
+    },
+    {
+        type: "input",
+        name: "instalDesc",
+        message: "Write a short step by step guide to installing your project."
+    },
+    {
+        type: "input",
+        name: "gitHubUsername",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your Email Address?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which licenses would you like to apply to your project?",
+        choices: ["GPL", "Apache", "BSD", "MIT", "None"]
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please specify any usage instructions for your project"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Provide an example on how to run tests on your project."
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How can users make contributions to your project?"
+    },
+]
