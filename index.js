b8ffe4711e32955229893c19f518f0a903383f71
James V <james.vanhoke@yahoo.com>
2021-03-19 18:55:56 -0700

    Psuedo code + Package.json generation

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// prompt for information about application
+
+// write user responses to template literals sample readme.MD
+//     Description, 
+//     Table of Contents, 
+//     Installation
+//     Usage
+//     License
+//     Contributing,
+//     Tests
+//     Questions
+
+// project title is displayed as the title of the README
+
+// choose a license from a list of options, print badge for that license to top of readme and add it to the license section
+
+//enter GitHub username add to the section of the README entitled Questions, with a link to the GitHub profile
+
+//enter email address add to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+//click on the links in the Table of Contents have it link to corresponding sections
